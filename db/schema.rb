80be9c3b703b0887ad81a72ffdcf1e4981eab838
Nick Hoffman <nick@deadorange.com>
2010-08-05 00:00:45 -0400

    Migrated the DBs.

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100805035324) do
+ActiveRecord::Schema.define(:version => 20100805035814) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
