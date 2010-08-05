e6e4d86edcd21fd42c798ea1df857d7877f87190
Nick Hoffman <nick@deadorange.com>
2010-08-05 01:06:41 -0400

    Unvendored acts_as_dashboard.

--- vendor/plugins/spec/acts_as_dashboard/widget_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.join File.dirname(__FILE__), '..', 'spec_helper'
-require File.join File.dirname(__FILE__), '..', 'shared', 'widget_behaviours'
-
-describe ActsAsDashboard::Widget do
-  it_should_behave_like 'an ActsAsDashboard Widget'
-end
